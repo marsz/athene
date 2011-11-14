4bffc4e4bfd563b8b95c100c5837c8f4330d4456
marsz <marsz330@gmail.com>
2011-11-14 15:39:28 +0800

    model post

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Post do
+  include DataMaker
+  before do
+    init_all_data
+  end
+  it {should belong_to(:site)}
+  it {should belong_to(:user)}
+  it {should validate_uniqueness_of(:site_post_id).scoped_to([:site_id])}
+  it {should validate_presence_of(:site_id)}
+  it {should validate_presence_of(:site_post_id)}
+  it {should validate_presence_of(:user_id)}
+  it {should validate_presence_of(:url)}
+  it {should validate_presence_of(:date)}
+  it {should validate_presence_of(:title)}
+end
