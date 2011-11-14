78e0c96be7a6bf6f47af4989413ecd19bd8bde01
marsz <marsz330@gmail.com>
2011-11-14 15:39:37 +0800

    model user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe User do
+  include DataMaker
+  before do
+    init_all_data
+  end
+  it {should belong_to(:site)}
+  it {should validate_uniqueness_of(:site_user_id).scoped_to([:site_id])}
+  it {should validate_presence_of(:site_id)}
+  it {should validate_presence_of(:site_user_id)}
+  it {should validate_presence_of(:url)}
+end
