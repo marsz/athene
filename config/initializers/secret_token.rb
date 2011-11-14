679c1b4340b16b1e63bda60e52373b36c3317d75
marsz <marsz330@gmail.com>
2011-11-14 13:41:53 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Athene::Application.config.secret_token = '4caf6720cbcfdee2383502f04e63e6862dd1d699069cfb4798b667e2c72597729548dac0ac9e4a3c9e84d676fc2a9b8e7b6a513e12da6cd4ee4908c49d1c0c40'
