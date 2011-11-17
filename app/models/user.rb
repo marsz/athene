9031237a82432b8f6ce7a05bb146837a15b22a0d
marsz <marsz330@gmail.com>
2011-11-17 18:38:13 +0800

    user has many post

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :site_user_id, :scope => [:site_id]
   validates_presence_of :site_id
   validates_presence_of :site_user_id
+  has_many :posts, :order => "date DESC,datetime DESC"
   # validates_presence_of :url
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,7 @@ describe User do
     init_all_data
   end
   it {should belong_to(:site)}
+  it {should have_many(:posts)}
   it {should validate_uniqueness_of(:site_user_id).scoped_to([:site_id])}
   it {should validate_presence_of(:site_id)}
   it {should validate_presence_of(:site_user_id)}
