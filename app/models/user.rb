e84f59c834f3a9caa603fecbc17aa46b3408748e
marsz <marsz330@gmail.com>
2011-11-17 13:32:57 +0800

    add counter cache

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  belongs_to :site
+  belongs_to :site, :counter_cache => true
   validates_uniqueness_of :site_user_id, :scope => [:site_id]
   validates_presence_of :site_id
   validates_presence_of :site_user_id
