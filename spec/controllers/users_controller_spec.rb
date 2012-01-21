405cc58fa14c599ce670e40102dcc868d53f304a
marsz <marsz330@gmail.com>
2012-01-21 16:17:42 +0800

    create user interface

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,6 +6,42 @@ describe UsersController do
   it { should route(:get, "/users").to(:action => :index) }
   it { should route(:get, "/users.json").to(:action => :index, :format => :json) }
   
+  describe "post create" do
+    before do
+      @site = Factory :site, :domain => :wretch
+    end
+    it "should be created with :site & :user" do
+      post :create, :site => @site.domain, :user => "marsz"
+      response.should redirect_to(user_path(assigns[:user]))
+    end
+    it "should not be created with exists data" do
+      user = Factory :user, :site_user_id => "marsz", :site => @site
+      post :create, :site => @site.domain, :user => user.site_user_id
+      hash = ActiveSupport::JSON.decode(response.body)
+      hash.key?("user").should be_true
+      hash["user"]["id"].should == user.id
+    end
+    it "should not be created with non-exists site" do
+      post :create, :site => :foo, :user => :bar
+      ActiveSupport::JSON.decode(response.body).should be_a_kind_of(Hash)
+    end
+  end
+  
+  describe "get show" do
+    before do
+      @user = Factory :user
+    end
+    it "exists user" do
+      get :show, :id => @user.id
+      ActiveSupport::JSON.decode(response.body).should_not be_nil
+    end
+    it "non-exists user" do
+      expect {
+        get :show, :id => (@user.id-1)
+      }.to raise_error
+    end
+  end
+  
   describe "get index" do
     def search_results opts = {}
       search(opts)[:users]
