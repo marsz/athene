78e35fee1c0157639787f9f4a0884cf3414e69cb
marsz <marsz330@gmail.com>
2012-02-21 20:19:55 +0800

    add path for rescue server

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,4 @@
+require "resque/server"
 if ["production","development"].include?(Rails.env)
   config = YAML.load_file("#{Rails.root}/config/resque.yml")[Rails.env]
   Resque.redis = config[:redis]
@@ -6,5 +7,4 @@ if ["production","development"].include?(Rails.env)
   rescue Errno::ECONNREFUSED
     puts "cant connect to redis server"
   end
-  
 end
